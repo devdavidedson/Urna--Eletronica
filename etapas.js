97f3462ce849b02759d101c6eafe1c12b9c4b4c5
David Lopes <104918465+devdavidedson@users.noreply.github.com>
2022-08-19 13:37:59 -0300

    Initial commit

---
+++ etapas.js
@@ -0,0 +1,68 @@
+let etapas = [
+    {
+        titulo: 'VEREADOR',
+        numeros: 5,
+        candidatos: [
+            {
+                numero: '77666',
+                nome: 'Arrochado',
+                partido: 'QQQ',
+                fotos: [
+                    {url: 'images/candidato4.png', legenda: 'vereador'}
+                ]
+            },
+            {
+                numero: '77444',
+                nome: 'Arrochadinho',
+                partido: 'WWW',
+                fotos: [
+                    {url: 'images/vice4.png', legenda: 'vereador'}
+                ]                
+            },
+            {
+                
+                numero: '77888',
+                nome: 'Arrochadão',
+                partido: 'EEE',
+                fotos: [
+                    {url: 'images/candidato3.png', legenda: 'vereador'}
+                ]
+            },
+            {
+                
+                numero: '77222',
+                nome: 'Arrochadaço',
+                partido: 'RRR',
+                fotos: [
+                    {url: 'images/vice3.png', legenda: 'vereador'}
+                ]
+            }
+        ]
+    },
+    {
+        titulo: 'PREFEITO',
+        numeros: 2,
+        candidatos: [
+            {
+                numero: '19',
+                nome: 'Arregou',
+                partido: 'TTT',
+                vice: 'Arregarei',
+                fotos: [
+                    {url: 'images/candidato1.png', legenda: 'Prefeito'},
+                    {url: 'images/vice1.png', legenda: 'Vice-Prefeito', pequeno: true}
+                ]
+            },
+            {
+                numero: '20',
+                nome: 'Gaiato',
+                partido: 'YYY',
+                vice: 'Gaiatinho',
+                fotos: [
+                    {url: 'images/candidato2.png', legenda: 'Prefeito'},
+                    {url: 'images/vice2.png', legenda: 'Vice-Prefeito', pequeno: true}
+                ]
+            }
+        ]
+    }
+]
